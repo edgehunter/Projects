006679ee882e8362262428754119ce0cc5469226
edgehunter <469589940@qq.com>
2017-09-06 17:19:56 +0800

    glut_CUDA_VBO_Fixed

---
+++ IMGUI_OpenGL_Glut_CUDA_VBO_Fixed/IMGUI_OpenGL_Glut_CUDA_VBO_Fixed/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// IMGUI_OpenGL_Glut_CUDA_VBO_Fixed.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
