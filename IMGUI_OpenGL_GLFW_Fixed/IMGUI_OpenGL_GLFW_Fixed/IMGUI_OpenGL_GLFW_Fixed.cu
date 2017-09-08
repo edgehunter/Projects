5954fe11e98c8ed7d8846911da9225f9d48ecb11
edgehunter <469589940@qq.com>
2017-09-08 14:39:57 +0800

    glfw_CUDA_VBO

---
+++ IMGUI_OpenGL_GLFW_Fixed/IMGUI_OpenGL_GLFW_Fixed/IMGUI_OpenGL_GLFW_Fixed.cu
@@ -0,0 +1,415 @@
+// IMGUI_OpenGL_GLFW_Fixed.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+//CUDA_VBO
+////////////////////////////////////////////////////////////////////////////////
+
+// includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#ifdef _WIN32
+#  define WINDOWS_LEAN_AND_MEAN
+#  define NOMINMAX
+#  include <windows.h>
+#endif
+
+// OpenGL Graphics includes
+#include <helper_gl.h>
+
+// includes, cuda
+#include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
+
+// Utilities and timing functions
+#include <helper_functions.h>    // includes cuda.h and cuda_runtime_api.h
+#include <timer.h>               // timing functions
+
+// CUDA helper functions
+#include <helper_cuda.h>         // helper functions for CUDA error check
+#include <helper_cuda_gl.h>      // helper functions for CUDA/GL interop
+
+//IMGUI GLFW
+////////////////////////////////////////////////////////////////////////////////
+// ImGui - standalone example application for Glfw + OpenGL 2, using fixed pipeline
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+
+#include <imgui.h>
+#include "imgui_impl_glfw.h"
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+
+
+//CUDA_VBO
+////////////////////////////////////////////////////////////////////////////////
+
+// GL functionality
+void createVBO(GLuint *vbo, struct cudaGraphicsResource **vbo_res,
+	unsigned int vbo_res_flags);
+void deleteVBO(GLuint *vbo, struct cudaGraphicsResource *vbo_res);
+
+// rendering callbacks
+void Render_CUDA_VBO();
+
+// Cuda functionality
+void runCuda(struct cudaGraphicsResource **vbo_resource);
+
+//CUDA_VBO
+////////////////////////////////////////////////////////////////////////////////
+
+// constants
+const unsigned int mesh_width = 256;
+const unsigned int mesh_height = 256;
+
+// vbo variables
+GLuint vbo;
+struct cudaGraphicsResource *cuda_vbo_resource;
+void *d_vbo_buffer = NULL;
+
+float g_fAnim = 0.0;
+#define MAX(a,b) ((a > b) ? a : b)
+
+
+///////////////////////////////////////////////////////////////////////////////
+//! Simple kernel to modify vertex positions in sine wave pattern
+//! @param data  data in global memory
+///////////////////////////////////////////////////////////////////////////////
+__global__ void simple_vbo_kernel(float4 *pos, unsigned int width, unsigned int height, float time)
+{
+	unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+	unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+
+	// calculate uv coordinates
+	float u = x / (float)width;
+	float v = y / (float)height;
+	u = u*2.0f - 1.0f;
+	v = v*2.0f - 1.0f;
+
+	// calculate simple sine wave pattern
+	float freq = 4.0f;
+	float w = sinf(u*freq + time) * cosf(v*freq + time) * 0.5f;
+
+	// write output vertex
+	pos[y*width + x] = make_float4(u, w, v, 1.0f);
+}
+
+
+void launch_kernel(float4 *pos, unsigned int mesh_width,
+	unsigned int mesh_height, float time)
+{
+	// execute the kernel
+	dim3 block(8, 8, 1);
+	dim3 grid(mesh_width / block.x, mesh_height / block.y, 1);
+	simple_vbo_kernel << < grid, block >> >(pos, mesh_width, mesh_height, time);
+}
+
+bool checkHW(char *name, const char *gpuType, int dev)
+{
+	cudaDeviceProp deviceProp;
+	cudaGetDeviceProperties(&deviceProp, dev);
+	strcpy(name, deviceProp.name);
+
+	if (!STRNCASECMP(deviceProp.name, gpuType, strlen(gpuType)))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int findGraphicsGPU(char *name)
+{
+	int nGraphicsGPU = 0;
+	int deviceCount = 0;
+	bool bFoundGraphics = false;
+	char firstGraphicsName[256], temp[256];
+
+	cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+
+	if (error_id != cudaSuccess)
+	{
+		printf("cudaGetDeviceCount returned %d\n-> %s\n", (int)error_id, cudaGetErrorString(error_id));
+		printf("> FAILED sample finished, exiting...\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// This function call returns 0 if there are no CUDA capable devices.
+	if (deviceCount == 0)
+	{
+		printf("> There are no device(s) supporting CUDA\n");
+		return false;
+	}
+	else
+	{
+		printf("> Found %d CUDA Capable Device(s)\n", deviceCount);
+	}
+
+	for (int dev = 0; dev < deviceCount; ++dev)
+	{
+		bool bGraphics = !checkHW(temp, (const char *)"Tesla", dev);
+		printf("> %s\t\tGPU %d: %s\n", (bGraphics ? "Graphics" : "Compute"), dev, temp);
+
+		if (bGraphics)
+		{
+			if (!bFoundGraphics)
+			{
+				strcpy(firstGraphicsName, temp);
+			}
+
+			nGraphicsGPU++;
+		}
+	}
+
+	if (nGraphicsGPU)
+	{
+		strcpy(name, firstGraphicsName);
+	}
+	else
+	{
+		strcpy(name, "this hardware");
+	}
+
+	return nGraphicsGPU;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//! Initialize CUDA_VBO
+////////////////////////////////////////////////////////////////////////////////
+bool init_CUDA_VBO()
+{
+	// initialize necessary OpenGL extensions
+	if (!isGLVersionSupported(2, 0))
+	{
+		fprintf(stderr, "ERROR: Support for necessary OpenGL extensions missing.");
+		fflush(stderr);
+		return false;
+	}
+
+	char *name_GraphicsGPU = new char[100];
+	int m_GraphicsGPU = findGraphicsGPU(name_GraphicsGPU);
+
+	if (m_GraphicsGPU)
+	{
+		printf("Find GraphicsGPU %d \n", m_GraphicsGPU);
+		printf("%s \n", name_GraphicsGPU);
+
+		cudaGLSetGLDevice(gpuGetMaxGflopsDeviceId());
+
+		// create VBO
+		createVBO(&vbo, &cuda_vbo_resource, cudaGraphicsMapFlagsWriteDiscard);
+
+		// run the cuda part
+		runCuda(&cuda_vbo_resource);
+
+		return true;
+
+	}
+
+	return false;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//! Run the Cuda part of the computation
+////////////////////////////////////////////////////////////////////////////////
+void runCuda(struct cudaGraphicsResource **vbo_resource)
+{
+	// map OpenGL buffer object for writing from CUDA
+	float4 *dptr;
+	checkCudaErrors(cudaGraphicsMapResources(1, vbo_resource, 0));
+	size_t num_bytes;
+	checkCudaErrors(cudaGraphicsResourceGetMappedPointer((void **)&dptr, &num_bytes,
+		*vbo_resource));
+	//printf("CUDA mapped VBO: May access %ld bytes\n", num_bytes);
+
+	// execute the kernel
+	//    dim3 block(8, 8, 1);
+	//    dim3 grid(mesh_width / block.x, mesh_height / block.y, 1);
+	//    kernel<<< grid, block>>>(dptr, mesh_width, mesh_height, g_fAnim);
+
+	launch_kernel(dptr, mesh_width, mesh_height, g_fAnim);
+
+	// unmap buffer object
+	checkCudaErrors(cudaGraphicsUnmapResources(1, vbo_resource, 0));
+}
+
+#ifdef _WIN32
+#ifndef FOPEN
+#define FOPEN(fHandle,filename,mode) fopen_s(&fHandle, filename, mode)
+#endif
+#else
+#ifndef FOPEN
+#define FOPEN(fHandle,filename,mode) (fHandle = fopen(filename, mode))
+#endif
+#endif
+
+
+////////////////////////////////////////////////////////////////////////////////
+//! Create VBO
+////////////////////////////////////////////////////////////////////////////////
+void createVBO(GLuint *vbo, struct cudaGraphicsResource **vbo_res,
+	unsigned int vbo_res_flags)
+{
+	assert(vbo);
+
+	// create buffer object
+	glGenBuffers(1, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, *vbo);
+
+	// initialize buffer object
+	unsigned int size = mesh_width * mesh_height * 4 * sizeof(float);
+	glBufferData(GL_ARRAY_BUFFER, size, 0, GL_DYNAMIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// register this buffer object with CUDA
+	checkCudaErrors(cudaGraphicsGLRegisterBuffer(vbo_res, *vbo, vbo_res_flags));
+
+	SDK_CHECK_ERROR_GL();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Delete VBO
+////////////////////////////////////////////////////////////////////////////////
+void deleteVBO(GLuint *vbo, struct cudaGraphicsResource *vbo_res)
+{
+
+	// unregister this buffer object with CUDA
+	checkCudaErrors(cudaGraphicsUnregisterResource(vbo_res));
+
+	glBindBuffer(1, *vbo);
+	glDeleteBuffers(1, vbo);
+
+	*vbo = 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Render_CUDA_VBO callback
+////////////////////////////////////////////////////////////////////////////////
+void Render_CUDA_VBO()
+{
+	// run CUDA kernel to generate vertex positions
+	runCuda(&cuda_vbo_resource);
+
+	// render from the vbo
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexPointer(4, GL_FLOAT, 0, 0);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glColor3f(1.0, 0.0, 0.0);
+	glDrawArrays(GL_POINTS, 0, mesh_width * mesh_height);
+	glDisableClientState(GL_VERTEX_ARRAY);
+
+	// Disable the vbo
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+
+
+//IMGUI glfw
+////////////////////////////////////////////////////////////////////////////////
+
+static void error_callback(int error, const char* description)
+{
+	fprintf(stderr, "Error %d: %s\n", error, description);
+}
+
+int main(int, char**)
+{
+	// Setup window
+	glfwSetErrorCallback(error_callback);
+	if (!glfwInit())
+		return 1;
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL2 example", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	// Init CUDA VBO
+	if (init_CUDA_VBO())
+	{
+		printf("Init CUDA_VBO Success!");
+	}
+
+	// Setup ImGui binding
+	ImGui_ImplGlfwGL2_Init(window, true);
+
+	// Load Fonts
+	// (there is a default font, this is only if you want to change it. see extra_fonts/README.txt for more details)
+	//ImGuiIO& io = ImGui::GetIO();
+	//io.Fonts->AddFontDefault();
+	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/Cousine-Regular.ttf", 15.0f);
+	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/DroidSans.ttf", 16.0f);
+	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyClean.ttf", 13.0f);
+	//io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyTiny.ttf", 10.0f);
+	//io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+
+	bool show_test_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImColor(114, 144, 154);
+
+	// Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+		ImGui_ImplGlfwGL2_NewFrame();
+
+		// 1. Show a simple window
+		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+		{
+			static float f = 0.0f;
+			ImGui::Text("Hello, world!");
+			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+			ImGui::ColorEdit3("clear color", (float*)&clear_color);
+			if (ImGui::Button("Test Window")) show_test_window ^= 1;
+			if (ImGui::Button("Another Window")) show_another_window ^= 1;
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		}
+
+		// 2. Show another simple window, this time using an explicit Begin/End pair
+		if (show_another_window)
+		{
+			ImGui::Begin("Another Window", &show_another_window);
+			ImGui::Text("Hello from another window!");
+			ImGui::End();
+		}
+
+		// 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+		if (show_test_window)
+		{
+			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver);
+			ImGui::ShowTestWindow(&show_test_window);
+		}
+
+		// Rendering
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Render_CUDA_VBO
+		g_fAnim += 0.01f;
+		Render_CUDA_VBO();
+
+		//glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound
+		
+		ImGui::Render();
+		glfwSwapBuffers(window);
+	}
+
+	// Cleanup
+	ImGui_ImplGlfwGL2_Shutdown();
+	glfwTerminate();
+
+	return 0;
+}
+
+
